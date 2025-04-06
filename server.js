78b981baf86fef9c6aa4fc2223e5ce379acb5809
Babun001 <babunroy987@gmail.com>
2025-04-06 15:20:03 +0530

    final backend

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+import dotenv from 'dotenv';
+dotenv.config({
+    path:"./.env"
+});
+
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
@@ -31,9 +36,28 @@ app.get('/api/tabsData', (req, res) => {
 })
 
 
+const adminAuth = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+  
+    if (!authHeader || !authHeader.startsWith("BabunRoy ")) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  
+    const base64 = authHeader.split(' ')[1];
+    const [user, pass] = Buffer.from(base64, 'base64').toString().split(':');
+  
+    if (user === process.env.ADMIN_USER && pass === process.env.ADMIN_PASS) {
+      next();
+    } else {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+  };
+  
+
+
 const datapath = path.resolve('./tabsData.json');
 
-app.post('/api/add-tab', async (req, res) => {
+app.post('/api/add-tab',  async (req, res) => {
     const newTab = req.body;
     // console.log(newTab);
 
@@ -68,6 +92,39 @@ app.post('/api/add-tab', async (req, res) => {
     }
 })
 
+
+
+app.delete('/api/delete-tabs', async (req, res) => {
+    const tabsToDelete = req.body.tabs;
+    console.log(tabsToDelete);
+    
+
+    if (!Array.isArray(tabsToDelete) || tabsToDelete.length === 0) {
+        return res.status(400).json({ message: 'No tabs provided for deletion' });
+    }
+
+    try {
+        const file = await fs.readFile(datapath, 'utf-8');
+        const currData = JSON.parse(file);
+
+        tabsToDelete.forEach(tab => {
+            if (currData[tab]) {
+                delete currData[tab];
+            }
+        });
+
+        await fs.writeFile(datapath, JSON.stringify(currData, null, 2));
+
+        res.status(200).json({
+            message: 'Tabs deleted successfully',
+            deleted: tabsToDelete
+        });
+
+    } catch (error) {
+        console.error('Error deleting tabs:', error);
+        res.status(500).json({ message: 'Server error while deleting tabs' });
+    }
+});
 
 
 
